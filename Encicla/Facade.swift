54d9f6e164d988c5373eb954bcc2eb96617e9c24
Mateo Echeverri <mate.echeverri@gmail.com>
2017-11-25 01:39:45 -0500

    ffgfgtg

---
+++ Encicla/Facade.swift
@@ -0,0 +1,9 @@
+//
+//  Facade.swift
+//  Encicla
+//
+//  Created by Mateo Echeverri on 11/24/17.
+//  Copyright © 2017 Mateo Echeverri. All rights reserved.
+//
+
+import Foundation
---
+++ Encicla/WindowView.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Encicla
+//
+//  Created by Mateo Echeverri on 11/24/17.
+//  Copyright © 2017 Mateo Echeverri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class WindowView: UIViewController, GMSDe
